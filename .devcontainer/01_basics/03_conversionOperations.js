7a534e8074bbbe62ea59fe5d6f3a078ce13436cd
robo-saurabh <140169386+robo-saurabh@users.noreply.github.com>
2024-01-03 19:07:39 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+// let score="45"
+// let score =null
+// let score =true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+
+// "45" -> 45
+// "45try" -> NAN
+// true = 1 , false = 0
+
+
+// let isLoggedIn = "saurabh"
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+//  1-> true; 0-> false
+// " " -> false
+//  "saurabh" -> true
+
+let someNumber = 34
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
