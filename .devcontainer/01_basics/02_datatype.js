980f28b1c8604be094aca6ac443f0b57abc8203e
robo-saurabh <140169386+robo-saurabh@users.noreply.github.com>
2024-01-03 19:08:26 +0000

    Conversion of data types

--- .devcontainer/01_basics/02_datatype.js
+++ .devcontainer/01_basics/02_datatype.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("saurabh")
 
 
-let name = "hitesh"
+let name = "saurabh"
 let age = 18
 let isLoggedIn = false
 let state;
