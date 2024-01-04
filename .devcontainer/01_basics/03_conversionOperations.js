2da794f75bf6d4d2642663bb91f90420863e23cb
Saurabh kumar <140169386+robo-saurabh@users.noreply.github.com>
2024-01-04 08:24:35 +0000

    Data type summery

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -22,5 +22,29 @@
 
 let someNumber = 34
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// #####  Operations  #####
+
+let value  = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "saurabh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
---
+++ .devcontainer/01_basics/05_datatypes-summery.js
@@ -0,0 +1,30 @@
+//  primitive data type
+
+//  7 types of premitive data type
+//  String, number, booleans, null, undefined, symbol, BigInt
+
+const score = 100 
+const scorevalue = 100.5
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;  // let userEmail= undefined 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id=== anotherId);
+const bigNumber = 3637889189478546n
+
+// Reference (Non primitive)
+// Array, Object, functions
+
+const city = ["Delhi", "Mumbai", "Banglore"];// Array  
+let myObj = {
+    name: "saurabh",
+    age: 22,
+}// Object
+
+const myFunction = function() {
+    console.log("hello world");
+}// Functions
+
+console.log(typeof myFunction);
