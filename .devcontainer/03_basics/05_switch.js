e7f93cce7e968b7da801041eed122ece18e9adfd
Saurabh kumar <140169386+robo-saurabh@users.noreply.github.com>
2024-01-10 12:07:27 +0000

    Switch

---
+++ .devcontainer/03_basics/05_switch.js
@@ -0,0 +1,52 @@
+//  Switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const month = "March"
+switch (month) {
+    case "January":
+        console.log("january");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
